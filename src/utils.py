ecdfdead15b3308de856eacd1937d97539aa09fd
LucasTHOMAS18 <maitre.renardowo@gmail.com>
2024-10-17 12:21:56 +0200

    rearrange

--- src/utils.py
+++ src/utils.py
@@ -4,3 +4,15 @@ def index_to_alpha(index: int) -> str:
 
 def alpha_to_index(alpha: str) -> int:
     return ord(alpha.upper()) - ord("A")
+
+
+def rearrange(matrix: list[list[str]], header: str) -> list[list[str]]:
+    current_header = list(sorted(header))
+    
+    res = []
+    header_indexes = [list(header).index(c) for c in current_header]
+    
+    for line_index in range(len(matrix)):
+        res.append([matrix[line_index][i] for i in header_indexes])
+    
+    return res
