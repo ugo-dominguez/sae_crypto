e4ba924ca15c80069cc4249fdf6386833c885005
ugodominguez <ugo.dominguez@etu.univ-orleans.fr>
2024-10-18 15:30:16 +0200

    add adfgvx utils

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,14 @@
+ADFGVX = "ADFGVX"
+
+
 def index_to_alpha(index: int) -> str:
     return chr((ord("A") + (index % 26)))
 
 
 def alpha_to_index(alpha: str) -> int:
     return ord(alpha.upper()) - ord("A")
+
+
+def get_matrix_ADFGVX(symbols) -> list[list[str]]:
+    ncols = len(ADFGVX)
+    return [symbols[i:i + ncols] for i in range(0, len(symbols), ncols)]
